5844024a9784eccfae91f105bbd219304831c056
Elanrif <saidbacoelanrif@gmail.com>
2024-12-19 23:30:14 +0100

    feat: add city

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    Hello Universe
+    Hello {{ city }}
   `,
   styles: `
    :host {

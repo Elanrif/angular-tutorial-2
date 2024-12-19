7a87fed3385a6a8bea1008df95a75164beacc2d5
Elanrif <saidbacoelanrif@gmail.com>
2024-12-19 23:46:40 +0100

    feat: Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,7 @@ import { UserComponent } from './user/user.component';
   selector: 'app-root',
   imports: [UserComponent, RouterOutlet],
   template: `
-    @for (user of users; track user.id){
-    <p>{{ user.name }}</p>
-    }
+    <div [contentEditable]="isEditable">hello world!</div>
   `,
   styles: `
    :host {
@@ -17,11 +15,5 @@ import { UserComponent } from './user/user.component';
   `,
 })
 export class AppComponent {
-  users = [
-    { id: 0, name: 'Sarah' },
-    { id: 1, name: 'Amy' },
-    { id: 2, name: 'Rachel' },
-    { id: 3, name: 'Jessica' },
-    { id: 4, name: 'Poornima' },
-  ];
+  isEditable = true;
 }

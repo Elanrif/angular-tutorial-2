93c0a27bec9a52506da5877171d6e3e84c8a10dc
Elanrif <saidbacoelanrif@gmail.com>
2024-12-20 17:23:43 +0100

    feat: Forms

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,20 @@
-import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [NgOptimizedImage],
+  imports: [FormsModule],
   template: `
-    User works!
+  <p>Username: {{username}}</p>
+  <p>{{username}}'s favorite framework: {{favoriteFramework}}</p>
+    <label for="framework">
+      Favorite Framework:
+      <input id="framework" type="text" [(ngModel)]="favoriteFramework" />
+    </label>
   `,
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
-
+  favoriteFramework = '';
+  username = 'Elanrif';
 }

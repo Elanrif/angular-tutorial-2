5710cb94e71742a9c23753eb5a3cee379d034a71
Elanrif <saidbacoelanrif@gmail.com>
2024-12-23 08:07:36 +0100

    feat: Formating data with pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { CarService } from './car.service';
+import { CurrencyPipe, DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet,DecimalPipe, DatePipe, CurrencyPipe],
   template: `
   <nav>
     <a [routerLink]="['/']">Home</a>
@@ -13,12 +14,22 @@ import { CarService } from './car.service';
   </nav>
   <router-outlet />
   <p> Car listing: {{ display }} </p>
+  <ul>
+    <li>Number with "decimal" {{num | number:"3.2-2"}}</li>
+    <li>Date with "date" {{birthday | date: 'medium'}}</li>
+    <li>Currency with "currency" {{ cost | currency }}</li>
+  </ul>
   `,
   styles: ` `,
 })
 export class AppComponent {
   display = '';
+  num = 103.1234;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
+
   constructor(private carService: CarService) {
     this.display = this.carService.getCars().join(' 🍥 ');
   }
+
 }
